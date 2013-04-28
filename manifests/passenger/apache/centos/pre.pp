1464eedd7f94b223ab92545e19e6e0edfc5d90bb
Benjamin Goldsbury <ben@inspiredbyspire.com>
2013-04-28 18:21:21 -0400

    Fixed paths; Changed dependencies for passenger to use puppetlabs-apache module

--- manifests/passenger/apache/centos/pre.pp
+++ manifests/passenger/apache/centos/pre.pp
@@ -1,6 +1,6 @@
 class rvm::passenger::apache::centos::pre {
   # Dependencies
-  if ! defined(Package['httpd'])       { package { 'httpd':       ensure => installed } }
-  if ! defined(Package['httpd-devel']) { package { 'httpd-devel': ensure => installed } }
-  if ! defined(Package['mod_ssl'])     { package { 'mod_ssl':     ensure => installed } }
+  require apache
+  require apache::mod::dev
+  require apache::mod::ssl
 }
